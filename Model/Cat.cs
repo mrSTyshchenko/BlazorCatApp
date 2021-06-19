a6758ce2aa50bdf0ca17c075a1259c087af5bb67
STyshchenko <88stysh88@gmail.com>
2021-06-18 21:00:46 -0500

    cat form added

---
+++ Model/Cat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace blazorTest.Model
+{
+    public class Cat
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int Age { get; set; }
+
+        //public Cat() { }
+
+        //public Cat(string Name, string LastName, int Age)
+        //{
+        //    this.Name = Name;
+        //    this.LastName = LastName;
+        //    this.Age = Age;
+        //}
+    }
+}
